8abe22d3ead4cc51dfda178aee0e842b60e6b128
Abdulsamed SAY <32686927+samedsay@users.noreply.github.com>
2020-05-15 01:39:50 +0200

    Update Solution.java

--- Arrays-101/Max Consecutive Ones/Solution.java
+++ Arrays-101/Max Consecutive Ones/Solution.java
@@ -1,1 +1,17 @@
-
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int counter = 0;
+        int max = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] == 1){
+                counter++;
+                if(counter > max){
+                    max = counter;
+                }
+            }else{
+                counter = 0;
+            }
+        }
+        return max;
+    }
+}
