2347e42ee55c61d2a851a836db60a804350caf86
Abdulsamed SAY <32686927+samedsay@users.noreply.github.com>
2020-05-21 03:08:46 +0200

    Create Solution.java

---
+++ Arrays-101/Check If N and Its Double Exist/Solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean checkIfExist(int[] arr) {
+        
+        int zeroCounter = 0;                   
+        for(int i = 0; i < arr.length; i++){            
+            if(arr[i] == 0){       
+                zeroCounter++;
+                if(zeroCounter > 1){
+                    return true;
+                }                 
+            }        
+            for(int j = 0; j < arr.length; j++){           
+                if(arr[i] == (arr[j] * 2) && arr[i] != arr[j]){              
+                    return true;
+                }
+            }
+        }
+        return false;   
+    }
+}
