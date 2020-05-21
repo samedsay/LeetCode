b8498d1c7a3f6f2fbbeacc5bba5a3abf4a071cc7
Abdulsamed SAY <32686927+samedsay@users.noreply.github.com>
2020-05-22 00:52:21 +0200

    Update Solution.java

--- Arrays-101/Move Zeroes/Solution.java
+++ Arrays-101/Move Zeroes/Solution.java
@@ -1,1 +1,13 @@
-
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int index = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] != 0){
+                int temp = nums[index];
+                nums[index] = nums[i];
+                nums[i] = temp;
+                index++;
+            }
+        }
+    }
+}
