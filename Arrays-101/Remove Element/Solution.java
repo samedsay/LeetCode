32459d0db95773d78d5213cd107476a9d0f622bc
Abdulsamed SAY <32686927+samedsay@users.noreply.github.com>
2020-05-21 02:38:03 +0200

    Update Solution.java

--- Arrays-101/Remove Element/Solution.java
+++ Arrays-101/Remove Element/Solution.java
@@ -3,8 +3,7 @@ class Solution {
         int len = 0;
         for(int i = 0; i < nums.length; i++){
             if(nums[i] != val){
-                nums[len] = nums[i];
-                len++;
+                nums[len++] = nums[i];
             }
         }
         return len;
